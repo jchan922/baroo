25f24163b3dc9a79e9f0ce8c7e1ae3a454140bcf
Justin Chan <jchan922@gmail.com>
2017-03-17 12:19:23 -0700

    added auto scroll. all animations are complete now.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,11 @@ $( document ).ready(function() {
     var menuList = document.getElementById('menu_list');
     var aboutPos = $("#nav_reappear").offset().top;
     var lastScroll = 0;
+    var extraHeight = $('#top').outerHeight();            // height of placeholder for auto scroll
 
+    ///////////////////
+    // Animate nav bar
+    ///////////////////
     $("#menu_left").click(function(){
         $(this).toggleClass("change");
         if(menu == false) {
@@ -16,8 +20,10 @@ $( document ).ready(function() {
         }
     });
 
+    ///////////////////
+    // Fade in nav bar
+    ///////////////////
     $( window ).scroll(function(){
-
         var scrollPos = $( window ).scrollTop();
         var navBarHeight = $("#nav_div").height();
         var navRepo = false;
@@ -61,8 +67,26 @@ $( document ).ready(function() {
                 });
             }
         }
-
         lastScroll = scrollPos
     })
+
+    ///////////////
+    // Auto scroll
+    ///////////////
+    $('.slide_section').click(function(event){
+        var linkHref = $(this).attr('href');                 // target item clicked
+        event.preventDefault();                              // turns off default a tag actions
+        // get value of element selected relative to the top minus extra height from nav bar
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top - 115
+        }, 1200);
+
+        $("#menu_list").css({"left": "-5000px", "transition": "2.0s"});
+        menu = false;
+        $(".change").removeClass();
+
+    });
+
+
 
 });
